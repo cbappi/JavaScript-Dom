c12d2135af0af6bfd49bc37fa1d68cd058d87ab0
cbappi <abdulwahedrr@gmail.com>
2023-10-07 17:16:24 +0600

    second

---
+++ button.js
@@ -0,0 +1,15 @@
+function addMe(drt){
+
+   alert(drt+100)
+}
+
+function say(){
+
+    alert("Are you looking for something")
+ }
+
+ var clickme = document.getElementById("clickme");
+
+ function checkAnother(){
+    clickme.src = "flowertwo.jpg";
+ }
